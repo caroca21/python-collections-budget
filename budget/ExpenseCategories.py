5a86ccd3dfb39291c3682b9de73862d49e70a300
caroca21 <goncalocaroca@gmail.com>
2022-10-09 17:00:01 +0100

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -5,9 +5,9 @@ def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
-    divide_set_comp = expenses.categorize_set_comprehension()
+    divided_set_comp = expenses.categorize_set_comprehension()
 
-    if not divide_set_comp == divided_for_loop:
+    if not divided_set_comp == divided_for_loop:
         print('Sets are NOT equal by == test')
 
     for a,b in zip(divided_for_loop, divided_set_comp):
